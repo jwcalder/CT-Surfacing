65102fcfc10e90a72a3dd3867a8fa128fd8aa713
Jeff Calder <jcalder@umn.edu>
2022-01-02 22:19:33 -0600

    update

--- dicom_firstpass.py
+++ dicom_firstpass.py
@@ -3,6 +3,5 @@ import amaazetools.dicom as dicom
 
 df = pd.read_csv('ScanLayout.csv')
 scanlayout = df[['CT','ScanPacket','CTHead2Tail','Mirrored','1','2','3','4']].copy()
-chopsheet = dicom.process_dicom('DICOM', scanlayout, CTdir='ScanOverviews', Meshdir='Meshes', 
-						     chopsheet=None, threshold=2000, padding=15)
+chopsheet = dicom.process_dicom('DICOM', scanlayout, CTdir='ScanOverviews', Meshdir='Meshes', chopsheet=None, threshold=2000, padding=15)
 chopsheet.to_csv('ChopLocations.csv', index=False)
--- dicom_refine.py
+++ dicom_refine.py
@@ -4,5 +4,4 @@ import amaazetools.dicom as dicom
 df = pd.read_csv('ScanLayout.csv')
 scanlayout = df[['CT','ScanPacket','CTHead2Tail','Mirrored','1','2','3','4']].copy()
 chopsheet = pd.read_csv('ChopLocations.csv')
-dicom.process_dicom('DICOM', scanlayout, CTdir='ScanOverviews', Meshdir='Meshes', 
-					 chopsheet=cropsheet, threshold=2000, padding=15)
+dicom.process_dicom('DICOM', scanlayout, CTdir='ScanOverviews', Meshdir='Meshes', chopsheet=cropsheet, threshold=2000, padding=15)
